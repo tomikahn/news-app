00045d08eb021bd16fdafd9b60ac1f3920babaa2
tomikahn <71043363+tomikahn@users.noreply.github.com>
2024-04-11 14:11:26 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     env:{
         API_URL: "https://newsapi.org/v2/everything",
-        API_KEY:"52c8ae91cebf4c92ae2420887a0a2271"
+        API_KEY:process.env.API_KEY
     }
 };
 
